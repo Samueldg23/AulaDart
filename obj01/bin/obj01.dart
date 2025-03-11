0564f439b8d4024c7c39f93dee8990f0737f4ca6
Samuel_Dg <samueldgomes00@gmail.com>
2025-03-10 23:29:35 -0300

    primeiro commit

---
+++ obj01/bin/obj01.dart
@@ -0,0 +1,5 @@
+import 'package:obj01/obj01.dart' as obj01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${obj01.calculate()}!');
+}
---
+++ obj01/test/obj01_test.dart
@@ -0,0 +1,8 @@
+import 'package:obj01/obj01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
